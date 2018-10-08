00cc6f64728e3ab0d46f0279974a8c2f655a2a1d
mrchaosbude <mail@chaosbude.com>
2018-10-08 18:56:55 +0200

    create return end give a dict back with the tons

--- trash-display/ajl.py
+++ trash-display/ajl.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
 import logging
 import urllib.request as requests
+import datetime
+import re
 
 logging.basicConfig(level=logging.DEBUG)
 
 class ajldates():
     def __init__(self):
         pass
-    
+
+    def run(self, city, street=None):
+        d = self.getCityUrl(city, street=None)
+        return self.getDates(d)
+
     def getCityUrl(self, city, street=None):
         """Gets the url of the given city
         
@@ -17,7 +23,7 @@ class ajldates():
         Returns:
             [str] -- [gets the url]
         """
-
+        logging.debug('[+] Start "getCityUrl" !')
         listofcitys = dict()
         data1 = requests.urlopen("https://www.ajl-mbh.de/abfallkalender/entsorgungstermine-ajl").read()        
         soup = BeautifulSoup(data1, "lxml")
@@ -51,10 +57,41 @@ class ajldates():
         logging.debug('[+] the Url= ' + listofcitys[city.casefold()] + "&street=" + listofstreets[street.casefold()] + "&printview=1")
         return listofcitys[city.casefold()] + "&street=" + listofstreets[street.casefold()] + "&printview=1"
 
-#    def getDates(self, url):
-#        page = 
+    def getDates(self, url):
+        logging.debug('[+] Start "getDates"')
+        page = requests.urlopen(url).read()
+        soup = BeautifulSoup(page, "lxml")
+        logging.debug('[+] Scrape "Year"')
+        global year
+        year = soup.find("h2", {"class": "ajl-green"})
+        year = re.search(r'\d{4}', year.text).group()
+
+        def gettonnes(name ,bsoup, text=0): # dont want to do this 4 times for every 
+            logging.debug('[+] Scrape "{}"'.format(name))
+            datetimelist = []
+            textlist = []
+            #year = soup.find("h2", {"class": "ajl-green"})
+            #year = re.search(r'\d{4}', year.text).group()
+            tonne = bsoup.find("div", {"class": name})
+            for item in tonne.find_all("div", {"class": "dayprint"}):                
+                textlist.append(item.text[3:] + str(year))
+                d = datetime.datetime.strptime(item.text[3:] + str(year), '%d.%m.%Y')
+                datetimelist.append(d)
+            if text == 1:
+                return textlist
+            return datetimelist
+
+        gelbeT = gettonnes("cat cat-gelb", soup)
+        papierT = gettonnes("cat cat-papier", soup)
+        bioT = gettonnes("cat cat-bio", soup)
+        SchwartzeT = gettonnes("cat cat-rest", soup)
+        logging.debug('[+] Finisch  Return the dict')
+        return {"Gelbe_Tonne" : gelbeT, "Papiertonne" : papierT, "Biotonne" : bioT, "Schwarze Tonne" : SchwartzeT}
+
     
 if __name__ == "__main__":
     d = ajldates()
-    p = d.getCityUrl("mützel", street="gorkistrasse")
-    #print (p)
+    #p = d.getCityUrl("mützel", street="gorkistrasse")
+    p = d.run("mützel", street="gorkistrasse")
+    #b = d.getDates("https://www.ajl-mbh.de/abfallkalender/entsorgungstermine-ajl?year=2018&town=154&street=265&printview=1")
+    print(p)
