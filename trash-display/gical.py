89bf1778e45869d1a9a4ef305c4663f2d8076426
Maik Thunack <mail@chaosbude.com>
2018-10-20 22:08:26 +0200

    versuche dten zu  bekommen

--- trash-display/gical.py
+++ trash-display/gical.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from ics import Calendar, timeline
 import credentials
+import arrow
 
 class gical():
     def get_Cal(self):
@@ -10,5 +11,5 @@ class gical():
 if __name__ == "__main__":
     ical_data = Calendar(urlopen(credentials.ical_url).read().decode('iso-8859-1'))
     e = timeline.Timeline(ical_data)
-    for i in e.today():
-        print(i.begin)
+    for i in e.included(arrow.now().replace(days=-1) ,arrow.now().replace(days=+1)):
+        print(type(i), i.name, i.begin ,i.end )
